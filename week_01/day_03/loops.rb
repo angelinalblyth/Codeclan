bb2fad28d84d760bf2f8357f0cb09321b4d9759b
Angelina Blyth <angelinalblyth@hotmail.com>
2018-05-16 14:52:16 +0100

    Added lesson on loops

---
+++ week_01/day_03/loops.rb
@@ -0,0 +1,14 @@
+# DRY - Dont Repeat Yourself
+#
+# WET - Write Everything Twice
+
+#WHILE LOOPS
+
+counter = 0
+
+my_number = 5
+
+while(counter < my_number)
+  p "Counter is #{counter}"
+  counter += 1
+end
---
+++ week_01/day_03/quiz.rb
@@ -0,0 +1,15 @@
+my_number = 5
+p "What number am I thinking of?"
+value = gets.chomp.to_i()
+
+while(value != my_number)
+  p "Nope, try again!"
+  if value < my_number
+    p "Too low"
+  else
+    p "Too High"
+  end
+  value = gets.chomp.to_i()
+end
+
+p "You got it!"
