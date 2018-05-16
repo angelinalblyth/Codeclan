f5cee4fd3f3ff8e534ddc495860a4f134ad0dda3
Angelina Blyth <angelinalblyth@hotmail.com>
2018-05-16 14:04:08 +0100

    Added pocketmoney task and symbols

--- week_01/day_03/hashes.rb
+++ week_01/day_03/hashes.rb
@@ -1,49 +1,75 @@
-#ways to make a hash
-my_first_hash = Hash.new()
-
-my_second_hash = {}
-
-
-meals = {
-  "breakfast" => "'yoghurt",
-  "lunch" => "greggs",
-  "dinner" => "steak"
-}
-
-p meals
-
-#left is keys right is values
-
-# silly_hash = {
-#   1 => "2",
-#   2 => "3",
-#   4 => false
+# #ways to make a hash
+# my_first_hash = Hash.new()
+#
+# my_second_hash = {}
+#
+#
+# meals = {
+#   "breakfast" => "'yoghurt",
+#   "lunch" => "greggs",
+#   "dinner" => "steak"
 # }
 #
-# p silly_hash
+# p meals
+#
+# #left is keys right is values
+#
+# # silly_hash = {
+# #   1 => "2",
+# #   2 => "3",
+# #   4 => false
+# # }
+# #
+# # p silly_hash
+#
+#
+# #print the value against the key
+# p meals["breakfast"]
+# #prints nil as there isnt the key in the hash
+# p meals["supper"]
+#
+#
+# #add a new value against the key key of supper, will return 0
+# # meals = Hash.new(0)
+# p meals["supper"]
+#
+# meals ["supper"] = "pancakes"
+#
+# p meals
+#
+#
+# #replace objects
+#
+# meals["dinner"] = "chicken chargrills"
+# p meals
+#
+# #delete a key/value
+#
+# meals.delete("breakfast")
+# p meals
 
 
-#print the value against the key
-p meals["breakfast"]
-#prints nil as there isnt the key in the hash
-p meals["supper"]
 
 
-#add a new value against the key key of supper, will return 0
-# meals = Hash.new(0)
-p meals["supper"]
-
-meals ["supper"] = "pancakes"
-
-p meals
 
 
-#replace objects
+# #Symbols
+# p :my_sym
+# p :hello
+#
+# p :my_sum + :hello
 
-meals["dinner"] = "chicken chargrills"
-p meals
+# meals = {
+#   :breakfast => "yogurt",
+#   :lunch => "Greggs"
+# }
+#
+# p meals[:breakfast]
 
-#delete a key/value
+#another way to write symbols
+countries = {
+  uk: "London",
+  germany: "Berlin",
+}
 
-meals.delete("breakfast")
-p meals
+p countries
--- week_01/day_03/pocketmoney.rb
+++ week_01/day_03/pocketmoney.rb
@@ -1,21 +1,25 @@
 #[TASK:] Make a hash with a key of a persons name and the value
 #as their pocket money. Try updating and deleting items from it.
 
-pocketmoney = {
-  "Angelina" => "2",
-  "Grant" => "10",
-  "Iona" => "20",
-  "Louise" => "0"
-}
-
-p pocketmoney
-
-#Add person
-pocketmoney["Abi"] = "5"
-p pocketmoney
-#Remove person
-pocketmoney.delete("Grant")
-p pocketmoney
-#Update persons amount
-pocketmoney["Iona"] = "3"
-p pocketmoney
+# pocketmoney = {
+#   "Angelina" => "2",
+#   "Grant" => "10",
+#   "Iona" => "20",
+#   "Louise" => "0"
+# }
+#
+# p pocketmoney
+#
+# #Add person
+# pocketmoney["Abi"] = "5"
+# p pocketmoney
+# #Remove person
+# pocketmoney.delete("Grant")
+# p pocketmoney
+# #Update persons amount
+# pocketmoney["Iona"] = "3"
+# p pocketmoney
+#
+#
+# p pocketmoney.keys
+# p pocketmoney.values
