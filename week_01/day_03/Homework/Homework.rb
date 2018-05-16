40df98c9bcd2a1cf6dfea617f0b4f42a5a40004f
Angelina Blyth <angelinalblyth@hotmail.com>
2018-05-16 16:08:31 +0100

    Added Edinburgh Waverley

--- week_01/day_03/Homework/Homework.rb
+++ week_01/day_03/Homework/Homework.rb
@@ -1,1 +1,6 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+
+#1. Add `"Edinburgh Waverley"` to the end of the array
+
+stops.push("Edinburgh Waverley")
+p stops
