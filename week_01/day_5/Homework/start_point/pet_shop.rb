d1c4ba2d8d601ef380eb92bfb4e4aea39e765dd6
Angelina Blyth <angelinalblyth@hotmail.com>
2018-05-20 18:36:31 +0100

    Added problem 17

--- week_01/day_5/Homework/start_point/pet_shop.rb
+++ week_01/day_5/Homework/start_point/pet_shop.rb
@@ -108,3 +108,9 @@ end
 def customer_pet_count(customer)
   customer[:pets].length
 end
+
+# 17th Problem
+
+def add_pet_to_customer(customer, new_pet)
+  customer[:pets].push(new_pet)
+end
--- week_01/day_5/Homework/start_point/specs/pet_shop_spec.rb
+++ week_01/day_5/Homework/start_point/specs/pet_shop_spec.rb
@@ -159,11 +159,11 @@ class TestPetShop < Minitest::Test
     assert_equal(0, count)
   end
 
-  # def test_add_pet_to_customer
-  #   customer = @customers[0]
-  #   add_pet_to_customer(customer, @new_pet)
-  #   assert_equal(1, customer_pet_count(customer))
-  # end
+  def test_add_pet_to_customer
+    customer = @customers[0]
+    add_pet_to_customer(customer, @new_pet)
+    assert_equal(1, customer_pet_count(customer))
+  end
 
   # # OPTIONAL
 
