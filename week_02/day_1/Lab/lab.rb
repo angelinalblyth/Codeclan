f00f8a6cb544971a0aa15ebb08a5d7ddb0ec63b8
Angelina Blyth <angelinalblyth@hotmail.com>
2018-05-21 14:02:47 +0100

    Added changing student name and cohort

--- week_02/day_1/Lab/lab.rb
+++ week_02/day_1/Lab/lab.rb
@@ -12,5 +12,13 @@ class Student
   def student_cohort()
     return @student_cohort
   end
-  
+
+  def update_student_name(new_name)
+    @student_name = new_name
+  end
+
+  def update_student_cohort(new_cohort)
+    @student_cohort = new_cohort
+  end
+
 end
--- week_02/day_1/Lab/specs/lap_spec.rb
+++ week_02/day_1/Lab/specs/lap_spec.rb
@@ -4,7 +4,7 @@ require_relative("../lab")
 
 class TestStudent < MiniTest::Test
 
-  def test_student_account
+  def test_student_name
     student_account = Student.new("Angelina", "G6")
     assert_equal("Angelina", student_account.student_name)
   end
@@ -12,6 +12,18 @@ class TestStudent < MiniTest::Test
   def test_student_cohort
     student_account = Student.new("Angelina", "G6")
     assert_equal("G6", student_account.student_cohort)
+  end
+
+  def test_update_student_name
+    student_account = Student.new("Angelina", "G6")
+    student_account.update_student_name("Grant")
+    assert_equal("Grant", student_account.student_name)
+  end
+
+  def test_update_student_cohort
+    student_account = Student.new("Angelina", "G6")
+    student_account.update_student_name("G7")
+    assert_equal("G7", student_account.student_name)
   end
 
 end
