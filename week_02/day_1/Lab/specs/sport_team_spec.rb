92c241528ed3d16ba894fb897d1a01bb1bec395d
Angelina Blyth <angelinalblyth@hotmail.com>
2018-05-21 14:32:32 +0100

    added test for returning team name

--- week_02/day_1/Lab/specs/sport_team_spec.rb
+++ week_02/day_1/Lab/specs/sport_team_spec.rb
@@ -4,4 +4,10 @@ require_relative("../sport_team")
 
 class TestSportTeam < MiniTest::Test
 
+  def test_team_name
+    sports_team = Team.new("Barbreck", ["Angelina", "Louise", "Christy", "Alex"], "Abi")
+    assert_equal("Barbreck", sports_team.team_name)
+  end
+
+
 end
--- week_02/day_1/Lab/sport_team.rb
+++ week_02/day_1/Lab/sport_team.rb
@@ -1,3 +1,15 @@
-class team
+class Team
+
+  # attr_reader :team_name, :players, :coach
+
+  def initialize(input_team_name, input_players, input_coach)
+    @team_name = input_team_name
+    @players = input_players
+    @coach = input_coach
+  end
+
+  def team_name()
+    return @team_name
+  end
 
 end
