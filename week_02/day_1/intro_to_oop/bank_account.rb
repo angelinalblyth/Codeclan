6f574d2f7dd7a10ead875ef8cb7feccee5319a46
Angelina Blyth <angelinalblyth@hotmail.com>
2018-05-21 11:34:36 +0100

    Added test to see if name, balance and type return values

--- week_02/day_1/intro_to_oop/bank_account.rb
+++ week_02/day_1/intro_to_oop/bank_account.rb
@@ -8,7 +8,21 @@ class BankAccount
   # Balance = 500
   # Type = business
   def initialize(input_name, input_balance, input_type)   # .new in the spec needs an initialize.
-
-
+    @holder_name = input_name # @symbol is global.
+    @balance = input_balance
+    @type = input_type
   end
+
+  def holder_name()
+    return @holder_name
+  end
+
+  def balance()
+    return @balance
+  end
+
+  def type()
+    return @type
+  end
+
 end
--- week_02/day_1/intro_to_oop/specs/bank_account_spec.rb
+++ week_02/day_1/intro_to_oop/specs/bank_account_spec.rb
@@ -6,6 +6,14 @@ class TestBankAccount < MiniTest::Test
 
   def test_account_name
     bank_account = BankAccount.new("John", 500, "business")
+    #matts_account = BankAccount.new("Matt", 100000, "personal")
+    #Can create multiple accounts just by using BankAccount.new
+    #and passing values into it
+
+    assert_equal("John", bank_account.holder_name)
+    #****capsulation***
+    assert_equal(500, bank_account.balance)
+    assert_equal("business", bank_account.type)
   end
 
 end
