6edfdcc062f470435969260827aabdfb2102a910
Angelina Blyth <angelinalblyth@hotmail.com>
2018-05-21 13:49:45 +0100

    Added method to return student name

--- week_02/day_1/Lab/lab.rb
+++ week_02/day_1/Lab/lab.rb
@@ -1,7 +1,12 @@
-class StudentAccount
+class Student
 
-  def initialize(student_name, student_cohort)
+  def initialize(input_student_name, input_student_cohort)
+    @student_name = input_student_name
+    @student_cohort = input_student_cohort
+  end
 
+  def student_name()
+    return @student_name
   end
 
 end
--- week_02/day_1/Lab/specs/lap_spec.rb
+++ week_02/day_1/Lab/specs/lap_spec.rb
@@ -5,7 +5,8 @@ require_relative("../lab")
 class TestStudent < MiniTest::Test
 
   def test_student_account
-    student_account = StudentAccount.new("Angelina", "G6")
+    student_account = Student.new("Angelina", "G6")
+    assert_equal("Angelina", student_account.student_name)
   end
 
 end
