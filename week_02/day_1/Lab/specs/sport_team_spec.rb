a089a8d0d0b705fbc8d7f944d0ac23e2b8c913d4
Angelina Blyth <angelinalblyth@hotmail.com>
2018-05-21 14:43:00 +0100

    added attr accessor to read and write

--- week_02/day_1/Lab/specs/sport_team_spec.rb
+++ week_02/day_1/Lab/specs/sport_team_spec.rb
@@ -19,9 +19,9 @@ class TestSportTeam < MiniTest::Test
     assert_equal("Abi", sports_team.coach)
   end
 
-  def test_set_coach
+  def test_set__new_coach
     sports_team = Team.new("Barbreck", ["Angelina", "Louise", "Christy", "Alex"], "Abi")
-    sports_team.update_coach("Scott")
+    sports_team.coach = "Scott"
     assert_equal("Scott", sports_team.coach)
   end
 
--- week_02/day_1/Lab/sport_team.rb
+++ week_02/day_1/Lab/sport_team.rb
@@ -1,6 +1,6 @@
 class Team
 
-  # attr_reader :team_name, :players, :coach
+attr_accessor :team_name, :players, :coach
 
   def initialize(input_team_name, input_players, input_coach)
     @team_name = input_team_name
@@ -8,21 +8,21 @@ class Team
     @coach = input_coach
   end
 
-  def team_name()
-    return @team_name
-  end
-
-  def players
-    return @players
-  end
-
-  def coach
-    return @coach
-  end
-
-  def update_coach(new_coach)
-    @coach = new_coach
-  end
+  # def team_name()
+  #   return @team_name
+  # end
+  #
+  # def players
+  #   return @players
+  # end
+  # #
+  # def coach
+  #   return @coach
+  # end
+  #
+  # def update_coach(new_coach)
+  #   @coach = new_coach
+  # end
 
 
 end
