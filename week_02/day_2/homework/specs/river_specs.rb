1825cd809412cdb40cac7df60d67245dea519618
Angelina Blyth <angelinalblyth@hotmail.com>
2018-05-22 18:38:29 +0100

    Added files and folder stucture. Also added the class River. First Test passing

---
+++ week_02/day_2/homework/specs/river_specs.rb
@@ -0,0 +1,16 @@
+require("minitest/autorun")
+require("minitest/rg")
+
+require_relative("../river")
+
+class RiverTest < MiniTest::Test
+
+  def setup
+    @river = River.new("Amazon")
+  end
+
+  def test_river_has_name
+    assert_equal("Amazon", @river.name)
+  end
+
+end
