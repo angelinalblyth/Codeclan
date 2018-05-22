57c9abe8ccb035e962824c7b0b9c2d0cc5d60354
Angelina Blyth <angelinalblyth@hotmail.com>
2018-05-22 18:46:27 +0100

    Added some more fish to fish spec

--- week_02/day_2/homework/specs/fish_specs.rb
+++ week_02/day_2/homework/specs/fish_specs.rb
@@ -6,7 +6,9 @@ require_relative("../fish")
 class FishTest < MiniTest::Test
 
   def setup
-    @fish = Fish.new("Nemo")
+    @fish1 = Fish.new("Nemo")
+    @fish2 = Fish.new("Clown")
+    @fish3 = Fish.new("Disco")
   end
 
   def test_fish_has_name
