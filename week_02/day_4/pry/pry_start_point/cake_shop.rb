0990660de553687a36b5323df606154601655789
Angelina Blyth <angelinalblyth@hotmail.com>
2018-05-24 13:33:43 +0100

    Added lesson on testing with pry

---
+++ week_02/day_4/pry/pry_start_point/cake_shop.rb
@@ -0,0 +1,26 @@
+require('pry')
+
+class CakeShop
+
+  attr_reader :cakes
+
+  def initialize(cakes)
+    @cakes = cakes
+  end
+
+  def average_cake_rating()
+
+    ratings = @cakes.map { |cake| cake.rating }
+    ratings_sum = 0
+
+    for rating in ratings
+      ratings_sum += rating
+    end
+
+    average = ratings_sum / ratings.length
+
+    return average
+
+  end
+
+end
